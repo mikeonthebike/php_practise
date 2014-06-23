230eeeb37a1732cace394fd1e12b461a95ddba81
miketeglas@gmail <mihalyteglas@192.168.0.101>
2014-06-22 21:53:13 -0400

    html css php

--- www/index.php
+++ www/index.php
@@ -1,8 +1,40 @@
 <html>
- <head>
-  <title>PHP Test</title>
- </head>
- <body>
- <?php echo '<p>Hello World</p>'; ?> 
- </body>
+<head>
+<style>
+.container {
+	margin-left: auto;
+	margin-right: auto;
+	width: 70%;
+	background-color: #b0e0e6;
+	pading-top: 3em;
+	margin-top: 3em;
+}
+
+p {
+	color: blue;
+}
+</style>
+<title>PHP Test</title>
+</head>
+
+<body>
+
+ <?php
+	echo '<p>Hello World</p>';
+	?>
+
+<?php
+	echo "<b>My first PHP script!</b>";
+?>
+ 
+ <div class="container">
+		<form action="welcome.php" method="post">
+			Name: <input type="text" name="name"><br> E-mail: <input type="text"
+				name="email"><br> <input type="submit">
+		</form>
+	</div>
+
+
+
+</body>
 </html>
